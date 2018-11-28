6819d739174452cbbdd7d3875f28dbc5adca6850
Salvatore Ventrone <ventrosky@gmail.com>
2018-11-28 22:35:20 +0100

    fixes
    adjusted the draft

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import os
 from flask import Flask, render_template, redirect, url_for, request, session
 from flask_sqlalchemy import SQLAlchemy
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit, send, join_room
 from werkzeug.security import generate_password_hash, check_password_hash
 from socket_module import GameRoomNamespace
+
 app = Flask(__name__)
 app.config['SECRET KEY'] = os.getenv("SABOTEUR_SECRET_KEY")
 app.config['SQLALCHEMY_DATABASE_URI'] = ''
@@ -11,37 +12,46 @@ db = SQLAlchemy(app)
 s = db.session
 socketio = SocketIO(app)
 game_rooms = {}
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-@socketio.on('create room')
+@socketio.on('create_room')
 def on_create(data):
-    # create game room, query game-manager.py for new game room
-    room_entry = {STUFF: "TO-BE DEFINED", gameState: "GAMEMANAGER To be used to generate new game listing", roomId:"generate", players: []}
-    roomId = room_entry.roomId
-    newGame = room_entry
-    game_rooms[roomId] = gameState
-    join_room(roomId)
-    emit('join_room', {'game_roomId': roomId})
+        print('Creating')
+        # create game room, query game-manager.py for new game room
+        room_entry = data#{STUFF: "TO-BE DEFINED", roomId: random_string(), players: []}
+        roomId = room_entry['roomId']
+        newGame = room_entry
+        game_rooms[roomId] = roomId
+        join_room(roomId)
+        emit('join_room', {'game_roomId': roomId})
 
 @socketio.on('join_room')
 def on_join(data):
-    # join a game room
-    roomId = data['game_roomId']
-    if roomId in game_rooms:
-        game_rooms[players].append(data['userId'])
-        join_room(roomId)
-        send(game_rooms[roomId].to_json(), roomId=roomId)
-        socketio.on_namespace(GameRoomNamespace(roomId))
+        print('Joining')
+        # join a game room
+        roomId = data['roomId']
+        if roomId in game_rooms:
+                game_rooms[roomId] # append(data['userId'])
+                join_room(roomId)
+                send(game_rooms[roomId], roomId=roomId)
+                socketio.on_namespace(GameRoomNamespace(roomId))
+        else:
+                emit('error', {'error': 'Unable to join room.'})
 
 @socketio.on("leave")
 def on_leave(roomId):
-    leave_room(roomId)
-    emit("leave game room", room=roomId)
+        leave_room(roomId)
+        emit("leave game room", room=roomId)
+
+def random_string():
+        # testing function
+        return ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(N))
 
 
 if __name__ == "__main__":
-    app.debug = True
-    app.run()
+        app.debug = True
+        app.run()
--- static/js/main.js
+++ static/js/main.js
@@ -1,38 +1,33 @@
 console.log('hello');
 
-
 $(() => {
 
-    var socket = io.connect('//127.0.0.1:5000/');
+    var socket = io.connect('http://' + document.domain + ':' + location.port);
 
     function load_game_rooms(data) {
         // to bind
         return false;
     }
 
-    function create_game_room(data) {
-        socket.emit('create_room', '/clicked to do');
-        return false;
-    }
-
-    function join_game_room() {
-        socket.emit('join', '/clicked to do');
-        return true;
-
-    }
-
     socket.on('connect', () => {
-       socket.emit('join', '/home?');
-
-       document.querySelector('#join_game_room').onclick = join_game_room;
-
+       console.log('Websocket connected!');
+       //socket.emit('join', '/home?');
     });
 
     
-    socket.on('ecc ecc', message_data => {
-      
+    socket.on('join_room', message_data => {
+      console.log(message_data);
         
     });
 
-
+    function createGame() {
+        console.log('Creating game...');
+        socket.emit('create_room', {STUFF: "TO-BE DEFINED", roomId: 'random_string()', players: []});
+    }
+    function joinGame() {
+        console.log('Joining game...');
+        socket.emit('join_room', {roomId: 'random_string()'});
+    }
+    document.querySelector('#create_game_room').onclick = createGame;
+    document.querySelector('#join_game_room').onclick = joinGame;
 });
