e59faa72e4ba31b4f27e87c0909e75b7c16a1612
Salvatore Ventrone <ventrosky@gmail.com>
2018-12-09 10:55:39 +0100

    events
    const response events to implement

--- classes/socket_module.py
+++ classes/socket_module.py
@@ -6,11 +6,38 @@ class GameLobbyNs(Namespace):
 
     game_rooms = {'roomId1': ["Jhon","Alex","Alice"],'roomId2': ["Bob"],'roomId3': ["Ted","Max"]}
 
+    RESPONSE_EVENTS = [
+        'round_result',
+        'new_round',
+        'score_gold',
+        'show_end_card',
+        'update_counters',
+        'gold_earned',
+        'gold_stolen',
+        'gold_card_earned',
+        'path_card_destroyed',
+        'show_goal_card',
+        'path_card_played',
+        'tool_status_changed',
+        'draw_new_cards',
+        'draw_new_role',
+        'give_cards',
+        'cards_discarded'
+    ]
+
     def make_rm_List(self):
         roomList = {}
         for key in self.game_rooms:
             roomList[key] = len(self.game_rooms[key])
         return roomList
+
+    def remove_player(self, userId, roomId):
+        self.game_rooms[roomId].remove(userId)
+        emit('roomsList',self.make_rm_List(), room='/lobby')
+
+    def add_player(self, userId, roomId):
+        self.game_rooms[roomId].append(userId)
+        emit('roomsList',self.make_rm_List(), room='/lobby')
 
     def on_connect(self):
         join_room('/lobby')
@@ -58,11 +85,11 @@ class GameLobbyNs(Namespace):
         print(request.sid + " joining " + data['roomId'])
         roomId = data['roomId']
         if (roomId in self.game_rooms) and (len(self.game_rooms[roomId]) < config.MAX_ROOM_SIZE)  and (data['userId'] not in self.game_rooms[roomId]):
-            self.game_rooms[roomId].append(data['userId'])
-            leave_room('/lobby')
+            self.add_player(data['userId'],data['roomId'])
+            leave_room(session.get('room'))
             join_room(roomId)
             send(self.game_rooms[roomId], roomId=roomId)
-            emit('roomsList',self.make_rm_List(), room='/lobby')
+            #emit('roomsList',self.make_rm_List(), room='/lobby')
         else:
             emit('error', {'error': 'Unable to join room.'})
 
@@ -73,8 +100,8 @@ class GameLobbyNs(Namespace):
 
     def on_leave(self, message):
         leave_room(message['room'])
+        print("leave")
         session['receive_count'] = session.get('receive_count', 0) + 1
         emit('my_response', {'data': 'In rooms: ' + ', '.join(rooms()), 'count': session['receive_count']})
-
 
 
