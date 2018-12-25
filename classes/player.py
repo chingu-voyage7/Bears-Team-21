540f6d96d8b1c39e4171af312ecdd2d3d97e7b79
Mihajlo Krsmanović <mihajlo.krsmanovic@yahoo.com>
2018-12-26 00:43:22 +0100

    Added player class

---
+++ classes/player.py
@@ -0,0 +1,35 @@
+class Player():
+    def __init__(self, name):
+        self.name = name
+        self.tools = {'pickaxe': True,
+                      'lamp': True,
+                      'cart': True}
+        self.cards = []
+        self.role = ''
+        self.gold = 0
+
+    def set_role(self, role):
+        self.role = role
+
+    def draw_card(self, card):
+        self.cards.append(card)
+
+    def play_card(self, index):
+        return self.cards.pop(index)
+
+    def break_tool(self, tool):
+        self.tools[tool] = False
+
+    def repair_tool(self, tool):
+        self.tools[tool] = True
+
+    def receive_gold(self, amount):
+        self.gold += amount
+
+    def reset(self):
+        self.tools = {'pickaxe': True,
+                      'lamp': True,
+                      'cart': True}
+        self.cards = []
+        self.role = ''
+        
