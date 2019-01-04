d1fe7f710bd215e2938563c8df807f715720af3d
Mihajlo Krsmanović <mihajlo.krsmanovic@yahoo.com>
2019-01-04 21:59:17 +0100

    Added crystal cards

--- classes/deck.py
+++ classes/deck.py
@@ -13,7 +13,7 @@ class Card:
         
 class PathCard(Card):
 
-    def __init__(self, name, edges):
+    def __init__(self, name, edges, crystal):
         super().__init__(name)
         self.connections = {-2: [], -1: [], 1: [], 2: []}
         self.edges = edges
@@ -22,6 +22,7 @@ class PathCard(Card):
                 self.connections[edge[0]].append(edge[1])
             if edge[1] < 3:
                 self.connections[edge[1]].append(edge[0])
+        self.crystal = int(crystal)
         self.set_required()
 
     def rotate(self):
@@ -76,7 +77,8 @@ class Deck:
                 if name in door: 
                     self.cards.append(DoorCard(name, edges, card['door']))
                 else:
-                    self.cards.append(PathCard(name, edges))
+                    crystal = 'crystal' in card
+                    self.cards.append(PathCard(name, edges, crystal))
             if name.startswith('action'):
                 type = card['type']
                 if 'tools' in card:
