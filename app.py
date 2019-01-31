a1e1cbb37465b295b0d3a2ba9af772ba60b30390
Salvatore Ventrone <ventrosky@gmail.com>
2019-01-31 01:22:43 +0100

    heroku port
    os.environ.get("PORT", 33507)

--- app.py
+++ app.py
@@ -132,6 +132,6 @@ def build_score(scores):
 
 if __name__ == "__main__":
     app.debug = False
-    #port = int(os.environ.get("PORT", 33507))
-    #app.run(host='0.0.0.0', port=port)
-    app.run()
+    port = int(os.environ.get("PORT", 33507))
+    app.run(host='0.0.0.0', port=port)
+    #app.run()
